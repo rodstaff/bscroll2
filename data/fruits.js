0f3ee7444417a4b25248b4b8502983ee6566558a
rodk <rod2010rod2010@hotmail.com>
2016-11-21 16:23:51 -0500

    added data, images folder

---
+++ data/fruits.js
@@ -0,0 +1,78 @@
+import Pic1 from "../public/images/fruits/pic1.jpg"
+
+export default function() {
+  return [
+    {
+      id: 1,
+      cname: "Apple",
+      lname: "Pyrus malus",
+      img:  ""
+    },
+    {
+      id: 2,
+      cname: "Banana",
+      lname: "Musa paradisicum",
+      img:  ""
+    },
+    {
+      id: 3,
+      cname: "Guava",
+      lname: "Psidium guava",
+      img:  ""
+    },
+    {
+      id: 3,
+      cname: "Jackfruit",
+      lname: "Artocarpus heterophyllus",
+      img:  ""
+    },
+    {
+      id: 4,
+      cname: "Lemon",
+      lname: "Citrus limonium",
+      img:  ""
+    },
+    {
+      id: 5,
+      cname: "Mango",
+      lname: "Mangifera indica",
+      img:  ""
+    },
+    {
+      id: 6,
+      cname: "Orange",
+      lname: "Citrus aurantium",
+      img:  ""
+    },
+    {
+      id: 7,
+      cname: "Papaya",
+      lname: "Carica papaya",
+      img:  ""
+    },
+    {
+      id: 8,
+      cname: "Pineapple",
+      lname: "Ananus sativus",
+      img:  ""
+    },
+    {
+      id: 9,
+      cname: "Pomegranate",
+      lname: "Punica granatum",
+      img:  ""
+    },
+    {
+      id: 11,
+      cname: "Tomato",
+      lname: "Lycopersican esculentum",
+      img:  ""
+    },
+    {
+      id: 12,
+      cname: "Watermelon",
+      lname: "Citrullus vulgaris",
+      img:  ""
+    }
+  ];
+};
---
+++ data/veggies.js
@@ -0,0 +1,77 @@
+import Pic1 from "../public/images/veggies/pic1.jpg"
+
+export default function() {
+  return [
+    {
+      id: 1,
+      cname: "Asparagus",
+      lname: "Asparagus officinalis",
+      img:  ""
+    },
+    {
+      id: 2,
+      cname: "Carrot",
+      lname: "Daucas carota",
+      img:  ""
+    },
+    {
+      id: 3,
+      cname: "Cauliflower",
+      lname: "Brassica oleracea",
+      img:  ""
+    },{
+      id: 4,
+      cname: "Cucumber",
+      lname: "Cucumis sativas",
+      img:  ""
+    },
+    {
+      id: 5,
+      cname: "Eggplant",
+      lname: "Solanum melongena",
+      img:  ""
+    },
+    {
+      id: 6,
+      cname: "Lettuce",
+      lname: "Latuca sativa",
+      img:  ""
+    },
+    {
+      id: 7,
+      cname: "Onion",
+      lname: "Allium cepa",
+      img:  ""
+    },
+    {
+      id: 8,
+      cname: "Pea",
+      lname: "Pisum sativum",
+      img:  ""
+    },
+    {
+      id: 9,
+      cname: "Potato",
+      lname: "Solanum tubersum",
+      img:  ""
+    },
+    {
+      id: 10,
+      cname: "Pumpkin",
+      lname: "Cucurbita moschata",
+      img:  ""
+    },
+    {
+      id: 11,
+      cname: "Radish",
+      lname: "Raphanus sativus",
+      img:  ""
+    },
+    {
+      id: 12,
+      cname: "Spinach",
+      lname: "Lactuca sativa",
+      img:  ""
+    },
+  ];
+};
